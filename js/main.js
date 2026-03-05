4de06e4334899a9405b57fa5452e2e5331bd1f21
cyberpravo225 <cyberpravo225@gmail.com>
2026-03-06 00:08:23 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -175,3 +175,21 @@ card.addEventListener('click', (e) => {
   });
 });
 })();
+/* =========================
+   Card hover light effect
+   ========================= */
+
+document.querySelectorAll('.card').forEach(card=>{
+
+  card.addEventListener('mousemove',(e)=>{
+
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty('--x', x + 'px');
+    card.style.setProperty('--y', y + 'px');
+
+  });
+
+});
