a1c0cf4e167ebc8ada6cf49f096432868671d3c4
cyberpravo225 <cyberpravo225@gmail.com>
2026-03-11 11:46:23 +0300

    фикшу работы выбора сложности

--- js/main.js
+++ js/main.js
@@ -263,24 +263,18 @@ card.innerHTML = `
 tipsContainer.appendChild(card);
 
 }
-   // открытие выбора сложности
+  // выбор сложности теста
 
-const testCards = document.querySelectorAll(".test-card")
-const modal = document.getElementById("difficultyModal")
-const closeBtn = document.querySelector(".modal-close")
+const testCards = document.querySelectorAll(".test-card");
+const modal = document.getElementById("difficultyModal");
+const closeBtn = document.querySelector(".modal-close");
 
-if(testCards){
 testCards.forEach(card=>{
 card.addEventListener("click",()=>{
-modal.classList.add("active")
-})
-})
-}
+modal.classList.add("active");
+});
+});
 
-if(closeBtn){
 closeBtn.addEventListener("click",()=>{
-modal.classList.remove("active")
-})
-}
-
-}
+modal.classList.remove("active");
+});
