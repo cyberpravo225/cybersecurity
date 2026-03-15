13bbbaf5d5935d38078bdd2a7182110e5c729611
cyberpravo225 <cyberpravo225@gmail.com>
2026-03-16 02:11:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -475,3 +475,22 @@ searchInput.dispatchEvent(new Event("input"))
 })
 
 }
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("visible")
+
+}
+
+})
+
+},{
+threshold:0.15
+})
+
+document.querySelectorAll(".reveal").forEach(el=>{
+observer.observe(el)
+})
